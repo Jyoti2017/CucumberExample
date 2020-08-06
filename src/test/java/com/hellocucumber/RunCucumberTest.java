a84ee9df5097174a17b8a582bf5a5c124a86a3fe
Juli <jyoti.puthal@gmail.com>
2020-08-06 15:45:55 +0530

    xml report generation

---
+++ src/test/java/com/hellocucumber/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package com.hellocucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin={"pretty","junit:target/cucumber-reports/Cucumber.xml"}, features="src/test/resources",monochrome = true)
+public class RunCucumberTest {
+}
