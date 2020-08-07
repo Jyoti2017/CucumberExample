b2c8f975048c8cf7d69944e2f9fec025c6db9bd0
Jyoti <jyoti.puthal@gmail.com>
2020-08-07 21:24:32 +0530

    added

--- src/test/java/com/hellocucumber/Stepdefs.java
+++ src/test/java/com/hellocucumber/Stepdefs.java
@@ -12,14 +12,14 @@ class IsItFriday {
 }
 
 public class Stepdefs {
+    
     private String today;
     private String actualAnswer;
 
-    
     @Given("today is {string}")
     public void today_is(String today) {
         this.today = today;
-    } 
+    }
 
     @When("I ask whether it's Friday yet")
     public void i_ask_whether_it_s_Friday_yet() {
