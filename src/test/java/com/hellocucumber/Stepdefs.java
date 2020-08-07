29e62b0675fb8c21e461d289adcabaeb33bcb4a8
Jyoti <jyoti.puthal@gmail.com>
2020-08-07 21:01:55 +0530

    Sequence change

--- src/test/java/com/hellocucumber/Stepdefs.java
+++ src/test/java/com/hellocucumber/Stepdefs.java
@@ -15,10 +15,7 @@ public class Stepdefs {
     private String today;
     private String actualAnswer;
 
-    @Given("today is {string}")
-    public void today_is(String today) {
-        this.today = today;
-    }
+
 
     @When("I ask whether it's Friday yet")
     public void i_ask_whether_it_s_Friday_yet() {
@@ -29,4 +26,9 @@ public class Stepdefs {
     public void i_should_be_told(String expectedAnswer) {
         assertEquals(expectedAnswer, actualAnswer);
     }
+    
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
+    }    
 }
