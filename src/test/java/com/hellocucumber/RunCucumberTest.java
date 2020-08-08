e752fa728888ff5cd4d60b8bc4c8e926113a13cd
Jyoti <jyoti.puthal@gmail.com>
2020-08-08 14:55:38 +0530

    Test

--- src/test/java/com/hellocucumber/RunCucumberTest.java
+++ src/test/java/com/hellocucumber/RunCucumberTest.java
@@ -5,6 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin={"pretty","junit:target/cucumber-reports/Cucumber.xml"},features="src/test/resources",monochrome = true)
+//@CucumberOptions(plugin={"pretty","junit:target/cucumber-reports/Cucumber.xml"},features="src/test/resources",monochrome = true)
+@CucumberOptions(plugin={"pretty","html:target/cucumber", "json:target/cucumber.json"},features="src/test/resources",monochrome = true)
 public class RunCucumberTest {
 }
